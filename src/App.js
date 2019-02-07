26196f5f852efec11e4575b983acecb67af4a66f
captbobo <cansurmeli@gmail.com>
2019-02-07 17:01:36 +0300

    refactor and feat: list, state, ref. header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-// import './App.css';
+import Map from './MapContainer'
+import List from './List'
+import { Geocoder } from "react-google-maps"
+
+import './App.css'
+
 
 class App extends Component {
-  render() {
-    console.log(process.env.REACT_APP_API_KEY)
-    return (
-      <div className="App">
-        <header>Bostanli Area Restaurants</header>
+  state = {
+    center: { lat: 38.4652132,
+              lng: 27.1030014 }
+  }
 
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <h1>Bostanli Area Restaurants</h1>
+        </header>
+        <div className="App">
+          <List />
+          <Map center={this.state.center}/>
+        </div>
         <footer>created by Can Sürmeli</footer>
       </div>
     );
